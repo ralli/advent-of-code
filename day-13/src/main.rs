2bed1c90723e4a55f4b99be7102f2b6ff6ab015d
Ralph Juhnke <r_juhnke@gmx.de>
2022-12-13 23:55:42 +0100

    day-13: much simpler PartialOrd implementation

--- day-13/src/main.rs
+++ day-13/src/main.rs
@@ -1,11 +1,7 @@
 extern crate core;
 
 use std::cmp::Ordering;
-use std::fmt;
-use std::fmt::Formatter;
 
-use itertools::EitherOrBoth;
-use itertools::Itertools;
 use nom::branch::alt;
 use nom::bytes::complete::tag;
 use nom::character::complete::line_ending;
@@ -31,14 +27,7 @@ fn part1(input: &str) -> usize {
     sequence_pairs
         .iter()
         .enumerate()
-        .map(|(i, (a, b))| {
-            // println!("== Pair {} ==", i + 1);
-            if a < b {
-                i + 1
-            } else {
-                0
-            }
-        })
+        .map(|(i, (a, b))| if a < b { i + 1 } else { 0 })
         .sum()
 }
 
@@ -59,98 +48,37 @@ fn part2(input: &str) -> usize {
 
     packets.sort();
 
-    // for p in packets.iter() {
-    //     println!("{}", p);
-    // }
-
     let first_idx = 1 + packets.iter().position(|x| x == &first).unwrap();
     let second_idx = 1 + packets.iter().position(|x| x == &second).unwrap();
 
     first_idx * second_idx
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 enum Item {
     List(Vec<Item>),
     Constant(i32),
 }
 
-fn compare(a: &Item, b: &Item) -> Ordering {
-    use Item::*;
-    let result = match (a, b) {
-        (Constant(a_value), Constant(b_value)) => a_value.cmp(b_value),
-        (List(a_list), List(b_list)) => a_list
-            .iter()
-            .zip_longest(b_list.iter())
-            .map(|x| match x {
-                EitherOrBoth::Right(_y) => {
-                    // println!("left is empty right {}", y);
-                    Ordering::Less
-                }
-                EitherOrBoth::Left(_y) => {
-                    // println!("right is empty left {}", y);
-                    Ordering::Greater
-                }
-                EitherOrBoth::Both(aaa, bbb) => compare(aaa, bbb),
-            })
-            .find(|&c| c != Ordering::Equal)
-            .unwrap_or(Ordering::Equal),
-        (List(_), Constant(b_value)) => {
-            let b_list = List(vec![Constant(*b_value)]);
-            compare(a, &b_list)
-        }
-        (Constant(a_value), List(_)) => {
-            let a_list = List(vec![Constant(*a_value)]);
-            compare(&a_list, b)
-        }
-    };
-
-    // println!("compare {} {} {:?}", a, b, result);
-
-    result
-}
-
 impl Ord for Item {
     fn cmp(&self, other: &Self) -> Ordering {
-        compare(self, other)
+        use Item::*;
+
+        match (self, other) {
+            (Constant(a), Constant(b)) => a.cmp(b),
+            (List(a), List(b)) => a.cmp(b),
+            (Constant(a), List(b)) => vec![Constant(*a)].cmp(b),
+            (List(a), Constant(b)) => {
+                let vec1 = vec![Constant(*b)];
+                a.cmp(&vec1)
+            }
+        }
     }
 }
 
 impl PartialOrd for Item {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(compare(self, other))
-    }
-}
-
-impl PartialEq for Item {
-    fn eq(&self, other: &Self) -> bool {
-        compare(self, other) == Ordering::Equal
-    }
-}
-
-impl Eq for Item {}
-
-impl Default for Item {
-    fn default() -> Self {
-        Item::Constant(i32::MIN)
-    }
-}
-
-impl fmt::Display for Item {
-    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        match self {
-            Item::Constant(n) => write!(f, "{}", n),
-            Item::List(lst) => {
-                write!(f, "[")?;
-                for (i, v) in lst.iter().enumerate() {
-                    if i > 0 {
-                        write!(f, ",")?;
-                    }
-                    write!(f, "{}", v)?;
-                }
-                write!(f, "]")
-            }
-        }
+        Some(self.cmp(other))
     }
 }
 
