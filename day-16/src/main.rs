b91d2735b5775d5abe3c5470c1f6616bc475f6da
Ralph Juhnke <r_juhnke@gmx.de>
2022-12-17 12:45:17 +0100

    cheat

--- day-16/src/main.rs
+++ day-16/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::{HashMap, HashSet, VecDeque};
+use std::collections::HashMap;
 
 use nom::branch::alt;
 use nom::bytes::complete::tag;
@@ -20,167 +20,71 @@ fn main() -> anyhow::Result<()> {
 }
 
 fn part1(input: &str) -> i32 {
-    let total_time = 30;
-
     let (_, nodes) = nodes(input).unwrap();
-    let weights: HashMap<&str, i32> = nodes.iter().map(|(s, f, _)| (*s, *f)).collect();
-    let adj: HashMap<&str, &Vec<&str>> = nodes.iter().map(|(s, _, n)| (*s, n)).collect();
-
-    let mut max_flow = 0;
-
-    let mut q = VecDeque::new();
-    let mut best: HashMap<&str, i32> = HashMap::new();
-
-    let start = "AA";
-    q.push_back((start, 0, total_time, HashSet::<&str>::new()));
-    // q.push_back((start, *f, total_time - 1, vec![start]));
-
-    while !q.is_empty() {
-        let (current, current_flow, time_left, opened) = q.pop_front().unwrap();
-
-        if current_flow > max_flow {
-            max_flow = current_flow;
-        }
-
-        if time_left > 0 && !opened.contains(&current) {
-            let w = *weights.get(current).unwrap();
-            if w > 0 {
-                let mut next_opened = opened.clone();
-                next_opened.insert(current);
-                let next_flow = (time_left - 1) * w + current_flow;
-                q.push_back((current, next_flow, time_left - 1, next_opened));
-            }
-        }
-
-        if time_left > 0 {
-            let next = *adj.get(current).unwrap();
-            for s in next.iter() {
-                let best_flow = best.entry(*s).or_insert(-1);
-                if current_flow > *best_flow {
-                    *best_flow = current_flow;
-                    q.push_back((s, current_flow, time_left - 1, opened.clone()));
-                }
-            }
-        }
-    }
-
-    max_flow
+    let mut solver = Solver::new();
+    solver.find_solution(&nodes)
 }
 
-type Move<'a> = (&'a str, &'a str, i32, i32, Vec<&'a str>);
-
-fn part2(input: &str) -> i32 {
-    let total_time = 30;
-
-    let (_, nodes) = nodes(input).unwrap();
-    let weights: HashMap<&str, i32> = nodes.iter().map(|(s, f, _)| (*s, *f)).collect();
-    let adj: HashMap<&str, &Vec<&str>> = nodes.iter().map(|(s, _, n)| (*s, n)).collect();
-
-    let mut max_flow = 0;
-
-    let mut q: VecDeque<Move> = VecDeque::new();
-    let mut best: HashMap<(&str, &str, Vec<&str>), i32> = HashMap::new();
-
-    let start = "AA";
-    q.push_back((start, start, 0, total_time, Vec::<&str>::new()));
-    // q.push_back((start, *f, total_time - 1, vec![start]));
-
-    while !q.is_empty() {
-        let (current, elephant_current, current_flow, time_left, opened) = q.pop_front().unwrap();
-
-        println!(
-            "{} {} {} {} {:?}",
-            current, elephant_current, current_flow, time_left, &opened
-        );
-
-        if current_flow > max_flow {
-            max_flow = current_flow;
-        }
-
-        if time_left > 0 && !opened.contains(&current) {
-            let w = *weights.get(current).unwrap();
-            if w > 0 {
-                let mut next_opened = opened.clone();
-                next_opened.push(current);
-                let next_flow = (time_left - 1) * w + current_flow;
-                let em = elephant_moves(
-                    current,
-                    elephant_current,
-                    next_flow,
-                    time_left,
-                    &next_opened,
-                    &weights,
-                    &adj,
-                );
-                for m in em {
-                    q.push_back(m);
-                }
-            }
-        }
-
-        if time_left > 0 {
-            let next = *adj.get(current).unwrap();
-            for s in next.iter() {
-                let em = elephant_moves(
-                    s,
-                    elephant_current,
-                    current_flow,
-                    time_left,
-                    &opened,
-                    &weights,
-                    &adj,
-                );
-                for m in em {
-                    let best_flow = best.entry((m.0, m.1, opened.clone())).or_insert(-1);
-                    let cf = m.3;
-                    if cf > *best_flow {
-                        *best_flow = cf;
-                        q.push_back(m);
-                    }
-                }
-            }
-        }
-    }
-
-    max_flow
+struct Solver<'a> {
+    cache: HashMap<(&'a str, Vec<&'a str>, i32), i32>,
 }
 
-fn elephant_moves<'a>(
-    current: &'a str,
-    elephant_current: &'a str,
-    current_flow: i32,
-    time_left: i32,
-    opened: &Vec<&'a str>,
-    weights: &HashMap<&'a str, i32>,
-    adj: &HashMap<&'a str, &Vec<&'a str>>,
-) -> Vec<Move<'a>> {
-    let mut result = Vec::new();
-
-    if !opened.contains(&elephant_current) {
-        let w = *weights.get(elephant_current).unwrap();
-        if w > 0 {
-            let mut next_opened = opened.clone();
-            next_opened.push(elephant_current);
-            let next_flow = (time_left - 1) * w + current_flow;
-            result.push((
-                current,
-                elephant_current,
-                next_flow,
-                time_left - 1,
-                next_opened,
-            ));
+impl<'a> Solver<'a> {
+    fn new() -> Self {
+        Solver {
+            cache: HashMap::new(),
         }
     }
 
-    let next = adj.get(elephant_current).unwrap();
-    for s in next.iter() {
-        result.push((current, s, current_flow, time_left - 1, opened.clone()));
+    fn find_solution(&mut self, nodes: &'a [Node]) -> i32 {
+        let node_map: HashMap<&str, &Node> = nodes.iter().map(|n| (n.from, n)).collect();
+        let open = Vec::new();
+        self.find("AA", &node_map, &open, 30)
     }
 
-    result
+    fn find(
+        &mut self,
+        id: &'a str,
+        node_map: &std::collections::HashMap<&str, &Node<'a>>,
+        open: &[&'a str],
+        time: i32,
+    ) -> i32 {
+        if time == 0 {
+            return 0;
+        }
+
+        let key = (id, open.to_vec(), time);
+        if let Some(r) = self.cache.get(&key) {
+            return *r;
+        }
+        let mut result = 0;
+        let node = node_map.get(id).unwrap();
+        if node.flow > 0 && !open.contains(&node.from) {
+            let mut next_open: Vec<&'a str> = open.to_owned();
+            next_open.push(node.from);
+            result =
+                result.max((time - 1) * node.flow + self.find(id, node_map, &next_open, time - 1));
+        }
+
+        for s in node.next_nodes.iter() {
+            result = result.max(self.find(s, node_map, open, time - 1));
+        }
+
+        self.cache.insert((id, open.to_vec(), time), result);
+        result
+    }
 }
 
-type Node<'a> = (&'a str, i32, Vec<&'a str>);
+fn part2(_input: &str) -> i32 {
+    todo!()
+}
+
+#[derive(Debug)]
+struct Node<'a> {
+    from: &'a str,
+    flow: i32,
+    next_nodes: Vec<&'a str>,
+}
 
 fn nodes(input: &str) -> IResult<&str, Vec<Node>> {
     separated_list1(line_ending, node)(input)
@@ -197,7 +101,14 @@ fn node(input: &str) -> IResult<&str, Node> {
     ))(input)?;
     let (input, next_nodes) = separated_list1(tag(", "), alpha1)(input)?;
 
-    Ok((input, (node, flow_rate, next_nodes)))
+    Ok((
+        input,
+        Node {
+            from: node,
+            flow: flow_rate,
+            next_nodes: next_nodes,
+        },
+    ))
 }
 
 fn read_file(filename: &str) -> anyhow::Result<String> {
