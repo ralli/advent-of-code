336c014e120e56a7fa905a86994dafb8104658c9
Ralph Juhnke <r_juhnke@gmx.de>
2022-12-25 22:50:27 +0100

    day-21 part 2

--- day-21/src/main.rs
+++ day-21/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::{HashMap, HashSet};
+
 use nom::branch::alt;
 use nom::bytes::complete::tag;
 use nom::character::complete;
@@ -6,7 +8,6 @@ use nom::combinator::map;
 use nom::multi::separated_list1;
 use nom::sequence::delimited;
 use nom::IResult;
-use std::collections::{HashMap, HashSet};
 
 fn main() -> anyhow::Result<()> {
     let input = read_file("./day-21/input.txt")?;
@@ -14,45 +15,104 @@ fn main() -> anyhow::Result<()> {
     let result = part1(&input);
     println!("{}", result);
 
-    // let result = part2(&input);
-    // println!("{}", result);
+    let result = part2(&input);
+    println!("{}", result);
 
     Ok(())
 }
 
 fn part1(input: &str) -> i64 {
     let (_, monkeys) = monkeys(input).unwrap();
-    let empty = HashMap::new();
-    let initial_values: Vec<_> = monkeys
-        .iter()
-        .filter_map(|monkey| {
-            monkey
-                .job
-                .evaluate(&empty)
-                .map(|result| (monkey.name, result))
-        })
-        .collect();
-    let mut context: HashMap<&str, i64> = HashMap::from_iter(initial_values.iter().copied());
-
-    loop {
-        let remaining: HashSet<_> = monkeys
-            .iter()
-            .filter(|n| !context.contains_key(n.name))
-            .filter_map(|m| m.job.evaluate(&context).map(|v| (m.name, v)))
-            .collect();
-
-        for (name, value) in remaining {
-            if name == "root" {
-                return value;
-            }
-            context.insert(name, value);
-        }
-        // println!("{:?}", context);
-    }
+    let monkey_map: HashMap<&str, &Monkey> = monkeys.iter().map(|m| (m.name, m)).collect();
+    find_solution("root", &monkey_map).unwrap()
 }
 
-fn part2(_input: &str) -> i64 {
-    todo!()
+fn part2(input: &str) -> i64 {
+    let (_, monkeys) = monkeys(input).unwrap();
+    let monkey_map: HashMap<&str, &Monkey> = monkeys.iter().map(|m| (m.name, m)).collect();
+    let root = monkey_map.get("root").unwrap();
+    let result = if let Job::Calculation(_, left, right) = root.job {
+        let left_contains_key = contains_key(left, "humn", &monkey_map);
+        if left_contains_key {
+            let right_value = find_solution(right, &monkey_map).unwrap();
+            find_goal(left, right_value, &monkey_map)
+        } else {
+            let left_value = find_solution(left, &monkey_map).unwrap();
+            find_goal(right, left_value, &monkey_map)
+        }
+    } else {
+        None
+    };
+    result.unwrap()
+}
+
+fn find_goal(name: &str, goal: i64, monkey_map: &HashMap<&str, &Monkey>) -> Option<i64> {
+    if name == "humn" {
+        return Some(goal);
+    }
+    let result = monkey_map.get(name).and_then(|monkey| match &monkey.job {
+        Job::Number(_) => Some(goal),
+        Job::Calculation(op, left, right) => {
+            let left_contains_key = contains_key(left, "humn", monkey_map);
+            if left_contains_key {
+                find_solution(right, monkey_map).and_then(|right_value| {
+                    let next_goal = match op {
+                        // l + r = g => l = g - r
+                        Op::Add => goal - right_value,
+                        // l - r = g => l = g + r
+                        Op::Sub => goal + right_value,
+                        // l * r = g => l = g / r
+                        Op::Mul => goal / right_value,
+                        // l / r = g => l = g * r
+                        Op::Div => goal * right_value,
+                    };
+                    find_goal(left, next_goal, monkey_map)
+                })
+            } else {
+                find_solution(left, monkey_map).and_then(|left_value| {
+                    let next_goal = match op {
+                        // l + r = g => r = g - l
+                        Op::Add => goal - left_value,
+                        // l - r = g => l - r - g = 0 =>  r = l - g
+                        Op::Sub => left_value - goal,
+                        // l * r = g => r = g / l
+                        Op::Mul => goal / left_value,
+                        // l / r = g => l = g * r => l / g = r
+                        Op::Div => left_value / goal,
+                    };
+                    find_goal(right, next_goal, monkey_map)
+                })
+            }
+        }
+    });
+
+    result
+}
+
+fn find_solution(name: &str, monkey_map: &HashMap<&str, &Monkey>) -> Option<i64> {
+    monkey_map.get(name).and_then(|monkey| match &monkey.job {
+        Job::Number(n) => Some(*n),
+        Job::Calculation(op, left_name, right_name) => find_solution(left_name, monkey_map)
+            .and_then(|left_value| {
+                find_solution(right_name, monkey_map)
+                    .map(|right_value| op.evaluate(left_value, right_value))
+            }),
+    })
+}
+
+fn contains_key(current: &str, name: &str, monkey_map: &HashMap<&str, &Monkey>) -> bool {
+    if current == name {
+        return true;
+    }
+    monkey_map
+        .get(current)
+        .map(|monkey| match monkey.job {
+            Job::Number(_) => false,
+            Job::Calculation(_, left, right) => {
+                contains_key(left, name, monkey_map) || contains_key(right, name, monkey_map)
+            }
+        })
+        .unwrap_or(false)
 }
 
 #[derive(Debug, Hash, Eq, PartialEq)]
@@ -63,28 +123,21 @@ enum Op {
     Div,
 }
 
+impl Op {
+    fn evaluate(&self, left_value: i64, right_value: i64) -> i64 {
+        match self {
+            Op::Add => left_value + right_value,
+            Op::Sub => left_value - right_value,
+            Op::Mul => left_value * right_value,
+            Op::Div => left_value / right_value,
+        }
+    }
+}
+
 #[derive(Debug, Hash, Eq, PartialEq)]
 enum Job<'a> {
     Number(i64),
     Calculation(Op, &'a str, &'a str),
-}
-
-impl<'a> Job<'a> {
-    fn evaluate(&self, context: &HashMap<&str, i64>) -> Option<i64> {
-        use Job::*;
-
-        match self {
-            Number(n) => Some(*n),
-            Calculation(op, lhs, rhs) => context.get(lhs).and_then(|left_value| {
-                context.get(rhs).map(|right_value| match op {
-                    Op::Add => *left_value + *right_value,
-                    Op::Sub => *left_value - *right_value,
-                    Op::Mul => *left_value * *right_value,
-                    Op::Div => *left_value / *right_value,
-                })
-            }),
-        }
-    }
 }
 
 #[derive(Debug, Hash, Eq, PartialEq)]
@@ -162,7 +215,7 @@ hmdt: 32";
     #[test]
     fn part2_works() {
         let result = part2(INPUT);
-        let expected = 1623178306i64;
+        let expected = 301;
         assert_eq!(result, expected);
     }
 }
