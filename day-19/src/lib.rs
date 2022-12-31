96bd92e5fddaa0b66fb4756666060b2b380c9727
Ralph Juhnke <r_juhnke@gmx.de>
2022-12-31 13:42:55 +0100

    day-19

--- day-19/src/lib.rs
+++ day-19/src/lib.rs
@@ -1,3 +1,5 @@
+use std::collections::{HashMap, HashSet, VecDeque};
+
 use nom::branch::alt;
 use nom::bytes::complete::tag;
 use nom::character::complete;
@@ -5,7 +7,6 @@ use nom::character::complete::{line_ending, space1};
 use nom::combinator::map;
 use nom::multi::separated_list1;
 use nom::IResult;
-use std::collections::HashMap;
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
 enum Material {
@@ -31,7 +32,7 @@ pub struct RobotDefinition {
 }
 
 impl RobotDefinition {
-    pub fn is_fullfilled(&self, num_ore: i32, num_clay: i32, num_obsidian: i32) -> bool {
+    pub fn is_fulfilled(&self, num_ore: i32, num_clay: i32, num_obsidian: i32) -> bool {
         num_ore >= self.costs_ore
             && num_clay >= self.costs_clay
             && num_obsidian >= self.costs_obsidian
@@ -47,7 +48,7 @@ pub struct BluePrint {
     pub geode: RobotDefinition,
 }
 
-#[derive(Debug, Copy, Clone, Default)]
+#[derive(Debug, Copy, Clone, Default, PartialEq, Eq, Hash)]
 pub struct State {
     pub step: i32,
 
@@ -63,40 +64,178 @@ pub struct State {
 }
 
 impl State {
+    pub fn next_state(&self, definition: &RobotDefinition, max_steps: i32) -> Option<State> {
+        assert!(self.num_geode >= 0);
+        assert!(self.num_ore >= 0);
+        assert!(self.num_clay >= 0);
+        assert!(self.num_obsidian >= 0);
+        if self.step == max_steps {
+            return None;
+        }
+        if self.can_produce(definition) {
+            return Some(self.produce(definition));
+        }
+
+        if definition.costs_ore > 0 && self.num_ore_bots == 0 {
+            return None;
+        }
+
+        let ore_steps = get_num_steps(self.num_ore_bots, self.num_ore, definition.costs_ore);
+
+        if definition.costs_clay > 0 && self.num_clay_bots == 0 {
+            return None;
+        }
+
+        let clay_steps = get_num_steps(self.num_clay_bots, self.num_clay, definition.costs_clay);
+
+        if definition.costs_obsidian > 0 && self.num_obsidian_bots == 0 {
+            return None;
+        }
+
+        let obsidian_steps = get_num_steps(
+            self.num_obsidian_bots,
+            self.num_obsidian,
+            definition.costs_obsidian,
+        );
+
+        let num_steps = ore_steps.max(clay_steps).max(obsidian_steps) + 1;
+
+        if self.step + num_steps > max_steps {
+            //return None;
+            let mut state = *self;
+            state.advance_num_steps(max_steps - self.step);
+            return Some(state);
+        }
+
+        let mut state = *self;
+        state.advance_num_steps(num_steps);
+        state.increase_bots(definition);
+
+        Some(state)
+    }
+
+    fn advance_num_steps(&mut self, num_steps: i32) {
+        self.step += num_steps;
+        self.num_ore += self.num_ore_bots * num_steps;
+        self.num_clay += self.num_clay_bots * num_steps;
+        self.num_obsidian += self.num_obsidian_bots * num_steps;
+        self.num_geode += self.num_geode_bots * num_steps;
+    }
+
+    fn increase_bots(&mut self, definition: &RobotDefinition) {
+        match definition.robot_type {
+            RobotType::Ore => {
+                self.num_ore_bots += 1;
+            }
+            RobotType::Clay => {
+                self.num_clay_bots += 1;
+            }
+            RobotType::Obsidian => {
+                self.num_obsidian_bots += 1;
+            }
+            RobotType::Geode => {
+                self.num_geode_bots += 1;
+            }
+        };
+        self.num_ore -= definition.costs_ore;
+        self.num_obsidian -= definition.costs_obsidian;
+        self.num_clay -= definition.costs_clay;
+    }
+
     pub fn produce(&self, definition: &RobotDefinition) -> State {
         let mut state = *self;
 
-        state.step += 1;
-        state.num_ore += state.num_ore_bots;
-        state.num_clay += state.num_clay_bots;
-        state.num_obsidian += state.num_obsidian_bots;
-        state.num_geode += state.num_geode_bots;
-
-        match definition.robot_type {
-            RobotType::Ore => {
-                state.num_ore_bots += 1;
-            }
-            RobotType::Clay => {
-                state.num_clay_bots += 1;
-            }
-            RobotType::Obsidian => {
-                state.num_obsidian_bots += 1;
-            }
-            RobotType::Geode => {
-                state.num_geode_bots += 1;
-            }
-        };
-
-        state.num_ore -= definition.costs_ore;
-        state.num_obsidian -= definition.costs_obsidian;
-        state.num_clay -= definition.costs_clay;
+        state.advance_num_steps(1);
+        state.increase_bots(definition);
 
         state
     }
 
     pub fn can_produce(&self, definition: &RobotDefinition) -> bool {
-        definition.is_fullfilled(self.num_ore, self.num_clay, self.num_obsidian)
+        definition.is_fulfilled(self.num_ore, self.num_clay, self.num_obsidian)
     }
+}
+
+pub fn max_geodes(&blueprint: &BluePrint, max_steps: i32) -> i32 {
+    let initial_state = State {
+        num_ore_bots: 1,
+        ..Default::default()
+    };
+    let mut q = VecDeque::from([initial_state]);
+    let mut max_geode = 0;
+
+    let robot_definitions = [
+        &blueprint.ore,
+        &blueprint.clay,
+        &blueprint.obsidian,
+        &blueprint.geode,
+    ];
+
+    let max_ore_bots = robot_definitions.iter().map(|d| d.costs_ore).max().unwrap();
+    let max_clay_bots = robot_definitions
+        .iter()
+        .map(|d| d.costs_clay)
+        .max()
+        .unwrap();
+    let max_obsidian_bots = robot_definitions
+        .iter()
+        .map(|d| d.costs_obsidian)
+        .max()
+        .unwrap();
+
+    let mut visited = HashSet::from([initial_state]);
+
+    while let Some(current) = q.pop_front() {
+        // assert!(q.len() < 100_000_000);
+        // println!("{:?}", current);
+
+        if current.num_geode > max_geode {
+            max_geode = current.num_geode;
+        }
+
+        if current.num_ore_bots < max_ore_bots {
+            if let Some(ore_state) = current.next_state(&blueprint.ore, max_steps) {
+                if visited.insert(ore_state) {
+                    q.push_back(ore_state);
+                }
+            }
+        }
+
+        if current.num_clay_bots < max_clay_bots {
+            if let Some(clay_state) = current.next_state(&blueprint.clay, max_steps) {
+                if visited.insert(clay_state) {
+                    q.push_back(clay_state);
+                }
+            }
+        }
+
+        if current.num_obsidian_bots < max_obsidian_bots {
+            if let Some(obsidian_state) = current.next_state(&blueprint.obsidian, max_steps) {
+                if visited.insert(obsidian_state) {
+                    q.push_back(obsidian_state);
+                }
+            }
+        }
+
+        if let Some(geode_state) = current.next_state(&blueprint.geode, max_steps) {
+            if visited.insert(geode_state) {
+                q.push_back(geode_state);
+            }
+        }
+    }
+
+    max_geode
+}
+
+fn get_num_steps(num_bots: i32, current: i32, goal: i32) -> i32 {
+    if num_bots == 0 {
+        return 0;
+    }
+    let mut i = 0;
+    while current + i * num_bots < goal {
+        i += 1;
+    }
+    i
 }
 
 pub fn blueprints(input: &str) -> IResult<&str, Vec<BluePrint>> {
--- day-19/src/main.rs
+++ day-19/src/main.rs
@@ -1,95 +1,34 @@
-use day_19::{blueprints, BluePrint, State};
-use std::collections::VecDeque;
+use day_19::{blueprints, max_geodes};
 
 fn main() -> anyhow::Result<()> {
-    let input = read_file("./day-19/test.txt")?;
+    let input = read_file("./day-19/input.txt")?;
 
     let result = part1(&input);
     println!("{}", result);
 
-    // let result = part2(&input);
-    // println!("{}", result);
+    let result = part2(&input);
+    println!("{}", result);
 
     Ok(())
 }
 
 fn part1(input: &str) -> i32 {
     let (_, blueprints) = blueprints(input).unwrap();
-    let geodes: Vec<i32> = blueprints.iter().map(|bp| bp.id * max_geodes(bp)).collect();
-    geodes.iter().sum()
-}
-
-fn max_geodes(&blueprint: &BluePrint) -> i32 {
-    let initial_state = State {
-        num_ore_bots: 1,
-        ..Default::default()
-    };
-    let mut q = VecDeque::from([initial_state]);
     let max_steps = 24;
-    let mut max_geode = 0;
-
-    let robot_definitions = [
-        &blueprint.ore,
-        &blueprint.clay,
-        &blueprint.obsidian,
-        &blueprint.geode,
-    ];
-
-    let max_ore_bots = robot_definitions.iter().map(|d| d.costs_ore).max().unwrap();
-    let max_clay_bots = robot_definitions
+    blueprints
         .iter()
-        .map(|d| d.costs_clay)
-        .max()
-        .unwrap();
-    let max_obsidian_bots = robot_definitions
-        .iter()
-        .map(|d| d.costs_obsidian)
-        .max()
-        .unwrap();
-
-    while let Some(current) = q.pop_front() {
-        assert!(q.len() < 100_000_000);
-        println!("{:?}", current);
-        if current.step == max_steps && current.num_geode > max_geode {
-            max_geode = current.num_geode;
-        }
-
-        if current.step > max_steps {
-            break;
-        }
-
-        if current.can_produce(&blueprint.geode) {
-            q.push_back(current.produce(&blueprint.geode))
-        }
-
-        if current.num_ore_bots < max_ore_bots && current.can_produce(&blueprint.ore) {
-            q.push_back(current.produce(&blueprint.ore))
-        }
-
-        if current.num_clay_bots < max_clay_bots && current.can_produce(&blueprint.clay) {
-            q.push_back(current.produce(&blueprint.clay))
-        }
-
-        if current.num_obsidian_bots < max_obsidian_bots && current.can_produce(&blueprint.obsidian)
-        {
-            q.push_back(current.produce(&blueprint.obsidian))
-        }
-
-        let mut next = current;
-        next.num_ore += next.num_ore_bots;
-        next.num_clay += next.num_clay_bots;
-        next.num_obsidian += next.num_obsidian_bots;
-        next.num_geode += next.num_geode_bots;
-        next.step += 1;
-
-        q.push_back(next);
-    }
-
-    max_geode
+        .map(|bp| bp.id * max_geodes(bp, max_steps))
+        .sum()
 }
 
 fn part2(input: &str) -> i32 {
-    todo!()
+    let (_, blueprints) = blueprints(input).unwrap();
+    let max_steps = 32;
+    blueprints
+        .iter()
+        .take(3)
+        .map(|bp| max_geodes(bp, max_steps))
+        .product()
 }
 
 fn read_file(filename: &str) -> anyhow::Result<String> {
@@ -107,6 +46,13 @@ mod tests {
     fn part1_works() {
         let result = part1(INPUT);
         let expected = 33;
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn part2_works() {
+        let result = part2(INPUT);
+        let expected = 12160;
         assert_eq!(result, expected);
     }
 }
