b9254764da194c2c9f0b1ea0e90bb1a04a47009a
Ralph Juhnke <r_juhnke@gmx.de>
2023-12-05 20:49:34 +0100

    2023 day-5 - more efficient calculation of part 2

--- 2023/day-5/src/main.rs
+++ 2023/day-5/src/main.rs
@@ -1,13 +1,13 @@
 use std::fs;
+use std::ops::Range;
 
 use anyhow::{anyhow, Context};
 use nom::bytes::complete::tag;
 use nom::character::complete;
 use nom::character::complete::{alpha1, line_ending, space1};
-use nom::IResult;
 use nom::multi::{many1, separated_list0};
 use nom::sequence::{delimited, preceded};
-use rayon::prelude::*;
+use nom::IResult;
 
 fn main() -> anyhow::Result<()> {
     let filename = "day-5.txt";
@@ -22,41 +22,54 @@ fn main() -> anyhow::Result<()> {
     Ok(())
 }
 
-fn part1(input: &str) -> anyhow::Result<u64> {
+fn part1(input: &str) -> anyhow::Result<i64> {
     let category_mappings = parse_input(input)?;
-    let result = category_mappings.seeds.iter().map(|value| category_mappings.location_value(*value)).min().unwrap_or_default();
+    let result = category_mappings
+        .seeds
+        .iter()
+        .map(|value| category_mappings.location_value(*value))
+        .min()
+        .unwrap_or_default();
     Ok(result)
 }
 
-fn part2(input: &str) -> anyhow::Result<u64> {
+fn part2(input: &str) -> anyhow::Result<i64> {
     let category_mappings = parse_input(input)?;
-    let mut result = u64::MAX;
-    // brute force + multi-threading with rayon
-    for v in category_mappings.seeds.chunks(2) {
-        let from = v[0];
-        let step = v[1];
-        let to = from + step;
-        result = (from..to).into_par_iter().map(|v| category_mappings.location_value(v)).min().unwrap().min(result);
+    let seed_ranges: Vec<Range<i64>> = category_mappings
+        .seeds
+        .chunks(2)
+        .map(|chunk| chunk[0]..chunk[0] + chunk[1])
+        .collect();
+    let mut results: Vec<i64> = Vec::new();
+
+    for seed_range in seed_ranges.into_iter() {
+        let mut ranges = vec![seed_range];
+        for mapping in category_mappings.category_mappings.iter() {
+            ranges = mapping.calculate_ranges(&ranges);
+        }
+        results.push(
+            ranges
+                .into_iter()
+                .map(|r| r.start)
+                .min()
+                .unwrap_or_default(),
+        );
     }
-    Ok(result)
+    Ok(results.iter().min().copied().unwrap())
 }
 
 #[derive(Debug)]
-struct SeedCategoryMappings {
-    seeds: Vec<u64>,
+struct Almanach {
+    seeds: Vec<i64>,
     category_mappings: Vec<CategoryMapping>,
 }
 
-impl SeedCategoryMappings {
-    fn location_value(&self, value: u64) -> u64 {
-        println!("value: {value}");
+impl Almanach {
+    fn location_value(&self, value: i64) -> i64 {
         let result = self.category_mappings.iter().fold(value, |v, m| {
             let result = m.range_value(v);
-            println!("{} {} {} {}", v, result, m.source, m.destination);
             result
         });
-        println!("result: {result}");
-        println!("---");
         result
     }
 }
@@ -69,38 +82,83 @@ struct CategoryMapping {
 }
 
 impl CategoryMapping {
-    fn range_value(&self, value: u64) -> u64 {
-        self.mappings.iter().find_map(|m| m.range_value(value)).unwrap_or(value)
+    fn calculate_ranges(&self, ranges: &[Range<i64>]) -> Vec<Range<i64>> {
+        let mut result = Vec::new();
+        let mut ranges = ranges.to_vec();
+        let mut next_ranges = Vec::new();
+
+        for mapping in self.mappings.iter() {
+            let source = mapping.source_start..(mapping.source_start + mapping.range_length);
+            while let Some(range) = ranges.pop() {
+                let before = range.start..source.start.min(range.end);
+                let between = range.start.max(source.start)..source.end.min(range.end);
+                let after = range.start.max(source.end)..range.end;
+
+                if before.start < before.end {
+                    next_ranges.push(before);
+                }
+
+                if between.start < between.end {
+                    let next_start = between.start + mapping.dest_start - source.start;
+                    let next_end = between.end + mapping.dest_start - source.start;
+                    result.push(next_start..next_end);
+                }
+
+                if after.start < after.end {
+                    next_ranges.push(after);
+                }
+            }
+            ranges.append(&mut next_ranges);
+        }
+        result.append(&mut ranges);
+        result
+    }
+
+    fn range_value(&self, value: i64) -> i64 {
+        self.mappings
+            .iter()
+            .find_map(|m| m.range_value(value))
+            .unwrap_or(value)
     }
 }
 
 #[derive(Debug)]
 struct Mapping {
-    destination_range_start: u64,
-    source_range_start: u64,
-    range_length: u64,
+    dest_start: i64,
+    source_start: i64,
+    range_length: i64,
 }
 
 impl Mapping {
-    fn range_value(&self, value: u64) -> Option<u64> {
-        if (self.source_range_start..self.source_range_start + self.range_length).contains(&value) {
-            println!("{}-{} {}", self.source_range_start, self.source_range_start + self.range_length, self.destination_range_start + (value - self.source_range_start));
-            return Some(self.destination_range_start + (value - self.source_range_start));
+    fn range_value(&self, value: i64) -> Option<i64> {
+        if (self.source_start..self.source_start + self.range_length).contains(&value) {
+            Some(self.dest_start + (value - self.source_start))
         } else {
             None
         }
     }
 }
 
-fn parse_input(input: &str) -> anyhow::Result<SeedCategoryMappings> {
-    let (_, category_mappings) = seed_category_mappings(input).map_err(|e| anyhow!(e.to_string()))?;
+fn parse_input(input: &str) -> anyhow::Result<Almanach> {
+    let (_, category_mappings) =
+        seed_category_mappings(input).map_err(|e| anyhow!(e.to_string()))?;
     Ok(category_mappings)
 }
 
-fn seed_category_mappings(input: &str) -> IResult<&str, SeedCategoryMappings> {
-    let (input, seeds) = delimited(preceded(tag("seeds:"), space1), separated_list0(space1, complete::u64), many1(line_ending))(input)?;
+fn seed_category_mappings(input: &str) -> IResult<&str, Almanach> {
+    let (input, seeds) = delimited(
+        preceded(tag("seeds:"), space1),
+        separated_list0(space1, complete::i64),
+        many1(line_ending),
+    )(input)?;
     let (input, category_mappings) = separated_list0(many1(line_ending), category_mapping)(input)?;
-    Ok((input, SeedCategoryMappings { seeds, category_mappings }))
+    Ok((
+        input,
+        Almanach {
+            seeds,
+            category_mappings,
+        },
+    ))
 }
 
 fn category_mapping(input: &str) -> IResult<&str, CategoryMapping> {
@@ -111,25 +169,31 @@ fn category_mapping(input: &str) -> IResult<&str, CategoryMapping> {
     let (input, _) = line_ending(input)?;
     let (input, mappings) = separated_list0(line_ending, mapping)(input)?;
 
-    Ok((input, CategoryMapping {
-        source: source.to_string(),
-        destination: destination.to_string(),
-        mappings,
-    }))
+    Ok((
+        input,
+        CategoryMapping {
+            source: source.to_string(),
+            destination: destination.to_string(),
+            mappings,
+        },
+    ))
 }
 
 fn mapping(input: &str) -> IResult<&str, Mapping> {
-    let (input, destination_range_start) = complete::u64(input)?;
+    let (input, destination_range_start) = complete::i64(input)?;
     let (input, _) = space1(input)?;
-    let (input, source_range_start) = complete::u64(input)?;
+    let (input, source_range_start) = complete::i64(input)?;
     let (input, _) = space1(input)?;
-    let (input, range_length) = complete::u64(input)?;
+    let (input, range_length) = complete::i64(input)?;
 
-    Ok((input, Mapping {
-        destination_range_start,
-        source_range_start,
-        range_length,
-    }))
+    Ok((
+        input,
+        Mapping {
+            dest_start: destination_range_start,
+            source_start: source_range_start,
+            range_length,
+        },
+    ))
 }
 
 #[cfg(test)]
@@ -188,7 +252,11 @@ humidity-to-location map:
 
     #[test]
     fn range_value_works() {
-        let m = Mapping { destination_range_start: 50, source_range_start: 98, range_length: 2 };
+        let m = Mapping {
+            dest_start: 50,
+            source_start: 98,
+            range_length: 2,
+        };
         assert_eq!(m.range_value(98), Some(50));
         assert_eq!(m.range_value(99), Some(51));
         assert_eq!(m.range_value(100), None);
