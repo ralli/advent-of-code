2c3d7f84e8b7c90b1c53b6a2ae86d8b6133eb50e
Juhnke_R <ralph.juhnke@f-i-sp.de>
2023-12-12 15:40:32 +0100

    2023 day 12 part2

--- 2023/day-12/src/main.rs
+++ 2023/day-12/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::BTreeMap;
 use std::fs;
 
 use anyhow::{anyhow, Context};
@@ -23,7 +24,7 @@ fn main() -> anyhow::Result<()> {
 
 fn part1(input: &str) -> anyhow::Result<i64> {
     let records = parse_input(input)?;
-    let result = records.iter().map(|r| count_arrangements(&r.spring_states, 0, &r.group_counts)).sum();
+    let result = records.iter().map(|r| count_arrangements(&r.spring_states, &r.group_counts)).sum();
     Ok(result)
 }
 
@@ -45,71 +46,91 @@ fn count_arrangements2(spring_states: &[SpringState], group_counts: &[i64]) -> i
         g.extend(group_counts.iter().copied());
     }
 
-    let result = count_arrangements(&s, 0, &g);
-    println!("...{result}");
+    let result = count_arrangements(&s, &g);
+    // println!("...{result}");
     result
 }
 
-fn count_arrangements(spring_states: &[SpringState], current_count: i64, group_counts: &[i64]) -> i64 {
-    // println!("{spring_states:?} {current_count} {group_counts:?}");
-    if spring_states.is_empty() && group_counts.is_empty() {
-        // println!("found!");
-        return 1;
-    }
-    if group_counts.is_empty() {
-        return if current_count == 0 && spring_states.iter().all(|&s| s == SpringState::Operational || s == SpringState::Unknown) {
+fn count_arrangements(spring_states: &[SpringState], group_counts: &[i64]) -> i64 {
+    fn count_arrangements_memo<'a>(spring_states: &'a [SpringState], current_count: i64, group_counts: &'a [i64], memo: &mut BTreeMap<(&'a [SpringState], i64, &'a [i64]), i64>) -> i64 {
+        if let Some(result) = memo.get(&(spring_states, current_count, group_counts)) {
+            return *result;
+        }
+        // println!("{spring_states:?} {current_count} {group_counts:?}");
+        if spring_states.is_empty() && group_counts.is_empty() {
             // println!("found!");
-            1
-        } else {
-            // println!("FALSE!");
-            0
-        };
-    }
-
-    let first_count = group_counts[0];
-    if first_count < current_count {
-        // println!("FALSE!");
-        return 0;
-    }
-    if spring_states.is_empty() {
-        return if current_count == first_count {
-            count_arrangements(spring_states, 0, &group_counts[1..])
-        } else {
-            // println!("FALSE!");
-            0
-        };
-    }
-
-    let first_state = spring_states[0];
-
-    let result = match first_state {
-        SpringState::Operational => {
-            if current_count == first_count {
-                count_arrangements(&spring_states[1..], 0, &group_counts[1..])
-            } else if current_count > 0 {
-                // println!("FALSE!");
-                0
-            } else {
-                count_arrangements(&spring_states[1..], 0, group_counts)
-            }
+            memo.insert((spring_states, current_count, group_counts), 1);
+            return 1;
         }
-        SpringState::Damaged => {
-            count_arrangements(&spring_states[1..], current_count + 1, group_counts)
-        }
-        SpringState::Unknown => {
-            let damaged = count_arrangements(&spring_states[1..], current_count + 1, group_counts);
-            let operational = if current_count == first_count {
-                count_arrangements(&spring_states[1..], 0, &group_counts[1..])
-            } else if current_count > 0 {
-                // println!("FALSE!");
-                0
+        if group_counts.is_empty() {
+            return if current_count == 0 && spring_states.iter().all(|&s| s == SpringState::Operational || s == SpringState::Unknown) {
+                // println!("found!");
+                memo.insert((spring_states, current_count, group_counts), 1);
+                1
             } else {
-                count_arrangements(&spring_states[1..], 0, group_counts)
+                // println!("FALSE!");
+                memo.insert((spring_states, current_count, group_counts), 0);
+                0
             };
-            damaged + operational
         }
-    };
-    result
+
+        let first_count = group_counts[0];
+        if first_count < current_count {
+            // println!("FALSE!");
+            memo.insert((spring_states, current_count, group_counts), 0);
+            return 0;
+        }
+        if spring_states.is_empty() {
+            return if current_count == first_count {
+                let result = count_arrangements_memo(spring_states, 0, &group_counts[1..], memo);
+                memo.insert((spring_states, current_count, group_counts), result);
+                result
+            } else {
+                // println!("FALSE!");
+                memo.insert((spring_states, current_count, group_counts), 0);
+                0
+            };
+        }
+
+        let first_state = spring_states[0];
+
+        let result = match first_state {
+            SpringState::Operational => {
+                if current_count == first_count {
+                    let result = count_arrangements_memo(&spring_states[1..], 0, &group_counts[1..], memo);
+                    result
+                } else if current_count > 0 {
+                    // println!("FALSE!");
+                    0
+                } else {
+                    let result = count_arrangements_memo(&spring_states[1..], 0, group_counts, memo);
+                    result
+                }
+            }
+            SpringState::Damaged => {
+                let result = count_arrangements_memo(&spring_states[1..], current_count + 1, group_counts, memo);
+                result
+            }
+            SpringState::Unknown => {
+                let damaged = count_arrangements_memo(&spring_states[1..], current_count + 1, group_counts, memo);
+                let operational = if current_count == first_count {
+                    let result = count_arrangements_memo(&spring_states[1..], 0, &group_counts[1..], memo);
+                    result
+                } else if current_count > 0 {
+                    // println!("FALSE!");damaged
+                    0
+                } else {
+                    let result = count_arrangements_memo(&spring_states[1..], 0, group_counts, memo);
+                    result
+                };
+                damaged + operational
+            }
+        };
+        memo.insert((spring_states, current_count, group_counts), result);
+        result
+    }
+    let mut memo: BTreeMap<(&[SpringState], i64, &[i64]), i64> = BTreeMap::new();
+    count_arrangements_memo(spring_states, 0, group_counts, &mut memo)
 }
 
 #[derive(Debug)]
@@ -119,7 +140,7 @@ struct ConditionRecord {
 }
 
 
-#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone, Copy)]
 enum SpringState {
     Operational,
     Damaged,
@@ -163,7 +184,7 @@ mod tests {
     fn test1() -> anyhow::Result<()> {
         let input = "???.### 1,1,3";
         let (_, record) = parse_condition_record(input).map_err(|e| anyhow!(e.to_string()))?;
-        let result = count_arrangements(&record.spring_states, 0, &record.group_counts);
+        let result = count_arrangements(&record.spring_states, &record.group_counts);
         let expected = 1;
         assert_eq!(result, expected);
         Ok(())
@@ -173,7 +194,7 @@ mod tests {
     fn test2() -> anyhow::Result<()> {
         let input = ".??..??...?##. 1,1,3";
         let (_, record) = parse_condition_record(input).map_err(|e| anyhow!(e.to_string()))?;
-        let result = count_arrangements(&record.spring_states, 0, &record.group_counts);
+        let result = count_arrangements(&record.spring_states, &record.group_counts);
         let expected = 4;
         assert_eq!(result, expected);
         Ok(())
