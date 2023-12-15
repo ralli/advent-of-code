9f3201a9160dfaf8e9ba16d70918a893526f85fd
Ralph Juhnke <r_juhnke@gmx.de>
2023-12-15 21:48:48 +0100

    2023 day 13 refactoring

--- 2023/day-15/src/main.rs
+++ 2023/day-15/src/main.rs
@@ -5,19 +5,20 @@ use nom::branch::alt;
 use nom::bytes::complete::{is_not, tag};
 use nom::character::complete;
 use nom::character::complete::char;
-use nom::IResult;
 use nom::multi::{separated_list0, separated_list1};
-use nom::Parser;
 use nom::sequence::preceded;
+use nom::IResult;
+use nom::Parser;
 
 fn main() -> anyhow::Result<()> {
     let filename = "day-15.txt";
     let input = fs::read_to_string(filename).with_context(|| format!("cannot load {filename}"))?;
+    let input = input.trim();
 
-    let result = part1(&input)?;
+    let result = part1(input)?;
     println!("{result}");
 
-    let result = part2(&input)?;
+    let result = part2(input)?;
     println!("{result}");
 
     Ok(())
@@ -26,76 +27,74 @@ fn main() -> anyhow::Result<()> {
 fn part1(input: &str) -> anyhow::Result<usize> {
     let (_, strings) = parse_groups(input).map_err(|e| anyhow!(e.to_string()))?;
 
-    let result = strings.iter().map(|s| hash(&s)).sum();
+    let result = strings.iter().map(|s| hash(s)).sum();
 
     Ok(result)
 }
 
 fn part2(input: &str) -> anyhow::Result<usize> {
-    let mut items = parse_input(input)?;
+    let commands = parse_input(input)?;
     let mut boxes: Vec<Box> = (0..256).map(|_| Box::default()).collect();
-    for item in items.iter_mut() {
-        let idx = hash(&item.label);
+    for cmd in commands.into_iter() {
+        let idx = hash(cmd.label);
         let b = &mut boxes[idx];
-        // println!("After: {:?}", item);
-        match item.operation {
+
+        match cmd.operation {
             Operation::Remove => {
-                if let Some((idx, _)) = b.items.iter().enumerate().find(|(_, i)| i.label == item.label) {
+                if let Some(idx) = b.items.iter().position(|i| i.label == cmd.label) {
                     b.items.remove(idx);
                 }
             }
-            Operation::Insert(_focal_length) => {
-                if let Some(it) = b.items.iter_mut().find(|i| i.label == item.label) {
-                    mem::swap(it, item);
+            Operation::Insert(focal_length) => {
+                if let Some(lens) = b.items.iter_mut().find(|l| l.label == cmd.label) {
+                    let mut new_lens = Lens {
+                        label: cmd.label,
+                        focal_length,
+                    };
+                    mem::swap(lens, &mut new_lens);
                 } else {
-                    b.items.push(item.clone());
+                    b.items.push(Lens {
+                        label: cmd.label,
+                        focal_length,
+                    });
                 }
             }
         };
         // print_boxes(&boxes);
     }
-    let mut result = 0;
-    for (box_idx, b) in boxes.iter().enumerate() {
-        for (item_idx, item) in b.items.iter().enumerate() {
-            let focal_length = match item.operation {
-                Operation::Insert(f) => f,
-                Operation::Remove => unreachable!()
-            };
-            result += (box_idx + 1) * (item_idx + 1) * focal_length as usize;
-        }
-    }
+    let result = boxes
+        .iter()
+        .enumerate()
+        .map(|(box_idx, b)| {
+            b.items
+                .iter()
+                .enumerate()
+                .map(|(item_idx, item)| (box_idx + 1) * (item_idx + 1) * item.focal_length as usize)
+                .sum::<usize>()
+        })
+        .sum();
     Ok(result)
 }
 
-fn print_boxes(boxes: &[Box]) {
-    for (i, b) in boxes.iter().enumerate() {
-        if !b.is_empty() {
-            println!("Box({}): {:?}", i, b);
-        }
-    }
-}
-
 fn hash(s: &str) -> usize {
-    s.bytes().fold(0usize, |n, c| {
-        ((n + c as usize) * 17) % 256
-    })
+    s.bytes().fold(0usize, |n, c| ((n + c as usize) * 17) % 256)
 }
 
 #[derive(Debug, Default)]
-struct Box {
-    items: Vec<Item>,
-}
-
-impl Box {
-    fn is_empty(&self) -> bool {
-        self.items.is_empty()
-    }
+struct Box<'a> {
+    items: Vec<Lens<'a>>,
 }
 
 #[derive(Debug, Clone)]
-struct Item {
-    label: String,
+struct Command<'a> {
+    label: &'a str,
     operation: Operation,
+}
+
+#[derive(Debug)]
+struct Lens<'a> {
+    label: &'a str,
+    focal_length: u32,
 }
 
 #[derive(Debug, Copy, Clone)]
@@ -104,19 +103,19 @@ enum Operation {
     Insert(u32),
 }
 
-fn parse_input(input: &str) -> anyhow::Result<Vec<Item>> {
-    let (_, items) = parse_items(input).map_err(|e| anyhow!(e.to_string()))?;
+fn parse_input(input: &str) -> anyhow::Result<Vec<Command>> {
+    let (_, items) = parse_commands(input).map_err(|e| anyhow!(e.to_string()))?;
     Ok(items)
 }
 
-fn parse_items(input: &str) -> IResult<&str, Vec<Item>> {
-    separated_list1(char(','), parse_item)(input)
+fn parse_commands(input: &str) -> IResult<&str, Vec<Command>> {
+    separated_list1(char(','), parse_command)(input)
 }
 
-fn parse_item(input: &str) -> IResult<&str, Item> {
+fn parse_command(input: &str) -> IResult<&str, Command> {
     let (input, label) = is_not("=-")(input)?;
     let (input, operation) = parse_operation(input)?;
-    Ok((input, Item { label: label.to_string(), operation }))
+    Ok((input, Command { label, operation }))
 }
 
 fn parse_operation(input: &str) -> IResult<&str, Operation> {
@@ -125,8 +124,8 @@ fn parse_operation(input: &str) -> IResult<&str, Operation> {
     alt((remove, insert))(input)
 }
 
-fn parse_groups(input: &str) -> IResult<&str, Vec<String>> {
-    separated_list0(tag(","), is_not(",").map(|b: &str| b.chars().filter(|&c| c != '\n').collect()))(input)
+fn parse_groups(input: &str) -> IResult<&str, Vec<&str>> {
+    separated_list0(tag(","), is_not(","))(input)
 }
 
 #[cfg(test)]
@@ -136,7 +135,6 @@ mod tests {
     use super::*;
 
     static INPUT: &str = r#"rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7"#;
-
 
     #[test]
     fn test1() -> anyhow::Result<()> {
