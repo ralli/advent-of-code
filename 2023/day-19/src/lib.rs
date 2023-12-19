c63aa93b3c72e07f593d33284ec6a638af010359
Juhnke_R <ralph.juhnke@f-i-sp.de>
2023-12-19 21:00:13 +0100

    2023 day 19 part 2

--- 2023/day-19/src/lib.rs
+++ 2023/day-19/src/lib.rs
@@ -22,12 +22,13 @@ pub fn part1(input: &str) -> anyhow::Result<i64> {
 pub fn part2(input: &str) -> anyhow::Result<i64> {
     let state = parse_input(input)?;
     let start = "in".to_string();
-    let mut ranges = all_combinations(&state.workflows, &start);
-    ranges.sort();
-    for r in ranges.iter() {
-        println!("{} {:?}", r.number_of_combinations(), r);
-    }
-    let result = ranges.iter().map(|r| r.number_of_combinations()).sum();
+    let ranges = Ranges {
+        x: (1, 4000),
+        m: (1, 4000),
+        a: (1, 4000),
+        s: (1, 4000),
+    };
+    let result = count_combinations(&state.workflows, &ranges, &start);
     Ok(result)
 }
 
@@ -35,39 +36,59 @@ fn state_for_part(workflows: &BTreeMap<String, Workflow>, part: &Part) -> String
     let mut start = "in".to_string();
 
     while let Some(workflow) = workflows.get(&start) {
-        start = workflow.rules.iter().filter_map(|r| r.matches(part)).next().unwrap();
+        start = workflow.rules.iter().filter_map(|r| r.next_rule_name(part)).next().unwrap();
     }
 
     start
 }
 
-fn all_combinations(workflows: &BTreeMap<String, Workflow>, name: &String) -> Vec<Ranges> {
-    let result = if name.as_str() == "A" {
-        vec![Ranges {
-            x: (1, 4000),
-            m: (1, 4000),
-            a: (1, 4000),
-            s: (1, 4000),
-        }]
-    } else if name.as_str() == "R" {
-        Vec::new()
+fn count_combinations(workflows: &BTreeMap<String, Workflow>, ranges: &Ranges, name: &String) -> i64 {
+    if name.as_str() == "R" {
+        0
+    } else if name.as_str() == "A" {
+        ranges.number_of_combinations()
     } else {
         let workflow = workflows.get(name).unwrap();
-
-        workflow.rules.iter().flat_map(|rule| {
-            let n = rule.name();
+        let mut ranges = *ranges;
+        let mut result = 0;
+        for rule in workflow.rules.iter() {
             match rule {
-                Rule::Basic { next, .. } => {
-                    all_combinations(workflows, next).iter().map(|range| rule.apply(range)).filter(|r| !r.is_empty()).collect::<Vec<Ranges>>()
+                Rule::Basic { name, op, value, next } => {
+                    let (lo, hi) = ranges.range_of(*name);
+
+                    let matching = match op {
+                        Op::Less =>
+                            (lo, hi.min(*value - 1)),
+                        Op::Greater =>
+                            (lo.max(*value + 1), hi)
+                    };
+
+                    let not_matching = match op {
+                        Op::Less =>
+                            (lo.max(*value), hi),
+                        Op::Greater =>
+                            (lo, hi.min(*value))
+                    };
+
+                    if matching.0 <= matching.1 {
+                        let mut copy = ranges;
+                        copy.set_range_of(*name, matching);
+                        result += count_combinations(workflows, &copy, next);
+                    }
+
+                    if not_matching.0 <= not_matching.1 {
+                        ranges.set_range_of(*name, not_matching)
+                    } else {
+                        break;
+                    }
                 }
                 Rule::Default { next } => {
-                    all_combinations(workflows, next).iter().map(|ranges| ranges.apply_all_rules_inverted(&workflow.rules)).filter(|r| !r.is_empty()).collect::<Vec<Ranges>>()
+                    result += count_combinations(workflows, &ranges, next);
                 }
             }
-        }).collect()
-    };
-    // println!("{}: {:?}", name, result);
-    result
+        }
+        result
+    }
 }
 
 
@@ -97,36 +118,7 @@ enum Rule {
 }
 
 impl Rule {
-    fn apply(&self, ranges: &Ranges) -> Ranges {
-        match self {
-            Rule::Default { .. } => *ranges,
-            Rule::Basic { name, op, value, next } => {
-                match op {
-                    Op::Less => {
-                        let mut ans = *ranges;
-                        let (a, b) = ans.range_of(*name);
-                        ans.set_range_of(*name, (a, b.min(*value - 1)));
-                        ans
-                    }
-                    Op::Greater => {
-                        let mut ans = *ranges;
-                        let (a, b) = ans.range_of(*name);
-                        ans.set_range_of(*name, (a.max(*value + 1), b));
-                        ans
-                    }
-                }
-            }
-        }
-    }
-
-    fn name(&self) -> String {
-        match self {
-            Rule::Basic { next, .. } => next.to_string(),
-            Rule::Default { next } => next.to_string()
-        }
-    }
-
-    fn matches(&self, part: &Part) -> Option<String> {
+    fn next_rule_name(&self, part: &Part) -> Option<String> {
         match self {
             Rule::Basic { name, op, value, next } => {
                 let part_value = part.value_of(*name);
@@ -187,50 +179,18 @@ struct Ranges {
     s: (i64, i64),
 }
 
-impl Ranges {
-    fn apply_all_rules_inverted(&self, rules: &[Rule]) -> Ranges {
-        let mut ans = *self;
-        for rule in rules.iter() {
-            match rule {
-                Rule::Default { .. } => {}
-                Rule::Basic { name, op, value, next: _ } => {
-                    match op {
-                        Op::Less => {
-                            let (a, b) = self.range_of(*name);
-                            ans.set_range_of(*name, (*value, b));
-                        }
-                        Op::Greater => {
-                            // >x => (a, b) = (a, x)
-                            let (a, b) = self.range_of(*name);
-                            ans.set_range_of(*name, (a, *value));
-                        }
-                    }
-                }
-            }
-        }
-        ans
-    }
-}
-
-fn is_range_empty(r: (i64, i64)) -> bool {
-    r.0 > r.1
-}
-
-fn range_combinations(r: (i64, i64)) -> i64 {
-    let (a, b) = r;
-    b - a + 1
-}
 
 impl Ranges {
     fn number_of_combinations(&self) -> i64 {
+        fn range_combinations(r: (i64, i64)) -> i64 {
+            let (a, b) = r;
+            b - a + 1
+        }
+
         range_combinations(self.x)
             * range_combinations(self.m)
             * range_combinations(self.a)
             * range_combinations(self.s)
-    }
-
-    fn is_empty(&self) -> bool {
-        is_range_empty(self.x) || is_range_empty(self.m) || is_range_empty(self.a) || is_range_empty(self.s)
     }
 
     fn range_of(&self, name: char) -> (i64, i64) {
@@ -300,7 +260,7 @@ fn parse_basic_rule(input: &str) -> IResult<&str, Rule> {
     let (input, value) = complete::i64(input)?;
     let (input, _) = tag(":")(input)?;
     let (input, next) = alpha1(input)?;
-    Ok((input, Rule::Basic { name: name, op, value, next: next.to_string() }))
+    Ok((input, Rule::Basic { name, op, value, next: next.to_string() }))
 }
 
 fn parse_default_rule(input: &str) -> IResult<&str, Rule> {
