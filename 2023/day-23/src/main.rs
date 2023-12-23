155455c5e529cfe86c6685d94432687c4b192a96
Ralph Juhnke <r_juhnke@gmx.de>
2023-12-23 22:50:19 +0100

    2023 day-23 part 2

--- 2023/day-23/src/main.rs
+++ 2023/day-23/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::VecDeque;
+use std::collections::{BTreeMap, BTreeSet, VecDeque};
 use std::fmt::Formatter;
 use std::str::FromStr;
 use std::{fmt, fs, iter};
@@ -8,8 +8,13 @@ use anyhow::{anyhow, Context};
 fn main() -> anyhow::Result<()> {
     let filename = "day-23.txt";
     let input = fs::read_to_string(filename).with_context(|| format!("cannot load {filename}"))?;
+
     let result = part1(&input)?;
     println!("{result}");
+
+    let result = part2(&input)?;
+    println!("{result}");
+
     Ok(())
 }
 
@@ -46,6 +51,130 @@ fn part1(input: &str) -> anyhow::Result<i64> {
     Ok(result as i64)
 }
 
+fn part2(input: &str) -> anyhow::Result<i64> {
+    let grid: Grid = input.parse()?;
+    let start = grid
+        .find_start()
+        .ok_or_else(|| anyhow!("no start position"))?;
+    let end = grid.find_end().ok_or_else(|| anyhow!("no end position"))?;
+    // longest path of a graph is NP-complete (see: https://en.wikipedia.org/wiki/Longest_path_problem)
+    // this graph has many nodes without branches. You can get a feasible runtime by compressing all such consecutive edges
+    // into a single one.
+    let graph = build_compressed_graph(&grid, &start, &end);
+    let mut visited = BTreeSet::new();
+    let result = dfs(&graph, &start, &end, &mut visited);
+    Ok(result)
+}
+
+fn build_compressed_graph(
+    grid: &Grid,
+    start: &Point,
+    end: &Point,
+) -> BTreeMap<Point, Vec<(Point, i64)>> {
+    let mut graph: BTreeMap<Point, Vec<(Point, i64)>> = BTreeMap::new();
+    // these are all nodes within the grid with more than two branches (where i come from + more than a single choice to move on)
+    let points = find_points_with_branches(grid, start, end);
+
+    //
+    // for each "branching point" find edges to the directly adjacent "branching points"
+    // the resulting graph is compressed, but contains silly cycles which are filtered out in the DFS step later on...
+    // see the attached mermaid diagram for the test input in the project
+    //
+    for &(start_row, start_col) in points.iter() {
+        let mut q: VecDeque<(Point, i64)> = VecDeque::from([((start_row, start_col), 0)]);
+        let mut visited = BTreeSet::from([(start_row, start_col)]);
+
+        while let Some(((row, col), distance)) = q.pop_front() {
+            // distance != 0 means "not start node"
+            if distance != 0 && points.contains(&(row, col)) {
+                graph
+                    .entry((start_row, start_col))
+                    .or_default()
+                    .push(((row, col), distance));
+            } else {
+                for (dr, dc) in DIRECTIONS.iter() {
+                    let (next_row, next_col) = (row + dr, col + dc);
+                    if next_row >= 0
+                        && next_row < grid.height
+                        && next_col >= 0
+                        && next_col < grid.width
+                        && grid.get(next_row, next_col) != '#'
+                        && visited.insert((next_row, next_col))
+                    {
+                        q.push_back(((next_row, next_col), distance + 1));
+                    }
+                }
+            }
+        }
+    }
+
+    graph
+}
+
+fn find_points_with_branches(grid: &Grid, start: &Point, end: &Point) -> Vec<Point> {
+    let mut points = vec![*start, *end];
+
+    for (row, line) in grid.cells.iter().enumerate() {
+        let row = row as isize;
+        for (col, _) in line.iter().enumerate().filter(|(_, c)| **c != '#') {
+            let col = col as isize;
+            let direction_count = DIRECTIONS
+                .iter()
+                .filter(|(dr, dc)| {
+                    let next_row = row + dr;
+                    let next_col = col + dc;
+                    next_row >= 0
+                        && next_row < grid.height
+                        && next_col >= 0
+                        && next_col <= grid.width
+                        && grid.get(next_row, next_col) != '#'
+                })
+                .count();
+            if direction_count > 2 {
+                points.push((row, col))
+            }
+        }
+    }
+    points
+}
+
+// must do it recurively since i do not know when to insert/remove the nodes from
+// visited otherwise...
+fn dfs(
+    graph: &BTreeMap<Point, Vec<(Point, i64)>>,
+    pos: &Point,
+    end: &Point,
+    visited: &mut BTreeSet<Point>,
+) -> i64 {
+    if pos == end {
+        0
+    } else {
+        visited.insert(*pos);
+        let edges = graph.get(pos).unwrap();
+        let mut m = i64::MIN;
+        for (p, d) in edges.iter() {
+            if !visited.contains(p) {
+                m = m.max(dfs(graph, p, end, visited) + *d);
+            }
+        }
+        visited.remove(pos);
+        m
+    }
+}
+
+fn print_mermaid_graph(graph: &BTreeMap<Point, Vec<(Point, i64)>>) {
+    println!("flowchart");
+    for ((sr, sc), edges) in graph.iter() {
+        for ((dr, dc), distance) in edges.iter() {
+            println!("  {sr}_{sc}[{sr},{sc}] --{distance}--> {dr}_{dc}[{dr},{dc}]");
+        }
+    }
+}
+
+static DIRECTIONS: [(isize, isize); 4] = [(-1, 0), (1, 0), (0, -1), (0, 1)];
+
+type Point = (isize, isize);
+type State = (Point, Point, i64);
 #[derive(Debug)]
 struct Grid {
     cells: Vec<Vec<char>>,
@@ -73,12 +202,25 @@ impl FromStr for Grid {
 }
 
 impl Grid {
+    fn get(&self, row: isize, col: isize) -> char {
+        self.cells[row as usize][col as usize]
+    }
+
     fn find_start(&self) -> Option<(isize, isize)> {
-        self.cells.iter().enumerate().find_map(|(row, line)| {
+        self.cells.first().and_then(|line| {
             line.iter()
                 .enumerate()
-                .find(|(_, ch)| **ch != '#')
-                .map(|(col, _)| (row as isize, col as isize))
+                .find(|(col, c)| **c == '.')
+                .map(|(col, _)| (0isize, col as isize))
+        })
+    }
+
+    fn find_end(&self) -> Option<(isize, isize)> {
+        self.cells.last().and_then(|line| {
+            line.iter()
+                .enumerate()
+                .find(|(col, c)| **c == '.')
+                .map(|(col, _)| (self.height - 1, col as isize))
         })
     }
 
@@ -137,6 +279,14 @@ mod tests {
     fn part1_works() -> anyhow::Result<()> {
         let result = part1(INPUT)?;
         let expected = 94;
+        assert_eq!(result, expected);
+        Ok(())
+    }
+
+    #[test]
+    fn part2_works() -> anyhow::Result<()> {
+        let result = part2(INPUT)?;
+        let expected = 154;
         assert_eq!(result, expected);
         Ok(())
     }
